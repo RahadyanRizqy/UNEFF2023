4e140000c0f31194dd492bb38c01bd03488103d7
razorlupical <lupicalnocturn@gmail.com>
2023-07-01 19:32:15 +0700

    test dari razor

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>@yield('title')</title>
+    <style>
+        * {
+            background-color: white;
+        }
+    </style>
 </head>
 <body>
     @yield('content')
